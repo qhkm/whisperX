bc14c8503fd48a5b7dabce31789def851113ecc5
qhkmdev90 <qhkmdev90@gmail.com>
2023-03-29 22:56:37 +0800

    change name again

--- whisperx/alignment.py
+++ whisperx/alignment.py
@@ -37,7 +37,7 @@ DEFAULT_ALIGN_MODELS_HF = {
     "fa": "jonatasgrosman/wav2vec2-large-xlsr-53-persian",
     "el": "jonatasgrosman/wav2vec2-large-xlsr-53-greek",
     "tr": "mpoyraz/wav2vec2-xls-r-300m-cv7-turkish",
-    "ms": "mesolitica/wav2vec2-xls-r-300m-mixed"
+    "ms": "malay-huggingface/wav2vec2-xls-r-1b-mixed"
 }
 
 
